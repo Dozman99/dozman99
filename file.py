6d0b722d5ffac9954cc52df9633badb07426e9d1
root <chiedozieonyekwum@gmail.com>
2021-07-26 06:35:58 +0100

    did small changes

---
+++ file.py
@@ -0,0 +1,9 @@
+# your link goes here
+link = "https://github.com/Dozman99/dozman99/blob/main/logos/python.png"
+
+# note: this will break if a repo/organization or subfolder is named "blob" -- would be ideal to use a fancy regex
+# to be more precise here
+print(link.replace("github.com", "raw.githubusercontent.com").replace("/blob/", "/"))
+
+# example output link:
+# https://raw.githubusercontent.com/knightlab-analyses/qurro-mackerel-analysis/master/AnalysisOutput/qurro-plot.qzv
